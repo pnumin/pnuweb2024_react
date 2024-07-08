0f535130153d19bac46c7026596246961e010e5f
pnumin <pnumin@pusan.ac.kr>
2024-07-08 10:57:31 +0900

    리액트 기초

---
+++ src/01/Hello.js
@@ -0,0 +1,9 @@
+function Hello() {
+  return(
+    <div>
+      안녕하세요!!
+    </div>
+  ); 
+}
+
+export default Hello ;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './01/Hello' ;
 
 function App() {
   return (
@@ -7,16 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/pnumin/pnuweb2024_react"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > 
+        김경민 
         </a>
+        </p> 
+        <Hello />
       </header>
     </div>
   );
