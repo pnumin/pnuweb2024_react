0c596d6c9c2b101b9dc9bc95aa80f22240bac959
pnumin <pnumin@pusan.ac.kr>
2024-07-09 11:58:26 +0900

    probs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 // import logo from './logo.svg';
 import './App.css';
-import Hello from './01/Hello' ;
+import { FaHome } from "react-icons/fa";
+
+// import Hello from './01/Hello' ;
+import MyDiv from './02/MyDiv';
+
 
 function App() {
   return (
     <div className="App-header">
-        <Hello /> 
+    <main className='flex flex-col justify-center items-center
+                      w-full md:w-4/5
+                      h-full'>
+      <header className='w-full h-16 bg-slate-200
+                         flex justify-between
+                         p-5 text-slate-800'>
+        <h1 className='text-xl font-bold'>리액트실습</h1>
+        <div><FaHome /></div>
+      </header>
+    <div className="w-full grow 
+                    flex flex-col justify-center items-center">
+          {/* <Hello />  */}
+          <MyDiv />
+      </div>
+      <footer className='w-full h-16
+                          flex justify-center items-center
+                         bg-black text-slate-200'>
+        <p className='text-sm font-bold'>
+          2024 여름 계절학기 소프트웨어융합기초1
+        </p>
+      </footer>
+    </main>
     </div>
   );
 }
